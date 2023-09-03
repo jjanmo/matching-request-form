80207ea930cb5c282161ad66bddc4fa248ec69de
jjanmo <jjanmo07@hanmail.net>
2023-09-03 18:10:23 +0900

    fix: styled-component props warning 수정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,23 +10,23 @@ interface Props {
 
 export default function Button({ variant, text, onClick }: Props) {
   return (
-    <SButton variant={variant} onClick={onClick}>
+    <SButton $variant={variant} onClick={onClick}>
       {text}
     </SButton>
   )
 }
 
-const SButton = styled.button<{ variant: Variant }>`
+const SButton = styled.button<{ $variant: Variant }>`
   width: 12rem;
   height: 5rem;
   margin: 0 0.5rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  color: ${({ variant }) => (variant === 'fill' ? colors.white : colors.green)};
-  background-color: ${({ variant }) => (variant === 'fill' ? colors.green : colors.white)};
+  color: ${({ $variant }) => ($variant === 'fill' ? colors.white : colors.green)};
+  background-color: ${({ $variant }) => ($variant === 'fill' ? colors.green : colors.white)};
   border-radius: 0.5rem;
-  border: ${({ variant }) => (variant === 'fill' ? `1px solid ${colors.green}` : `1px solid ${colors.gray03}`)};
+  border: ${({ $variant }) => ($variant === 'fill' ? `1px solid ${colors.green}` : `1px solid ${colors.gray03}`)};
   font-size: 1.6rem;
   font-weight: 500;
   cursor: pointer;
