726457679cd65f956019923d89ba810b320c89d3
jjanmo <jjanmo07@hanmail.net>
2023-09-03 18:13:39 +0900

    feat: 체크박스 체크 로직 추가

--- src/components/InputForm/InputForm.tsx
+++ src/components/InputForm/InputForm.tsx
@@ -1,14 +1,30 @@
 import { Item } from '@store/slice/formSlice.type'
 import * as S from './InputForm.style'
+import { useAppDispatch } from '@store/root'
+import { resultActions } from '@store/slice/resultSlice'
 
-function CheckboxForm({ title, options }: Item) {
+function CheckboxForm({ title, options, itemId }: Item) {
+  const dispatch = useAppDispatch()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id } = e.target
+    dispatch(resultActions.changeResultItem({ itemId, optionId: Number(id) }))
+  }
+
   return (
     <S.Container>
       <S.Question>{title}</S.Question>
       <S.OptionsContainer>
         {options.map((option) => (
-          <label htmlFor={String(option.id)}>
-            <input key={option.id} id={String(option.id)} type="checkbox" value={option.text} />
+          <label key={option.id} htmlFor={String(option.id)}>
+            <S.Checkbox />
+            <input
+              id={String(option.id)}
+              type="checkbox"
+              name={String(itemId)}
+              value={option.text}
+              onChange={handleChange}
+            />
             {option.text}
           </label>
         ))}
