f4305318697b30eff4d2ca8f9afec46832314148
jjanmo <jjanmo07@hanmail.net>
2023-09-04 06:21:03 +0900

    chores: 테마색 변경

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,7 @@ export const colors = {
   white: '#fff',
   black: '#323232',
   gray01: '#525252',
-  gray02: '#999',
+  gray02: '#C7C7C7',
   gray03: '#e1e1e1',
   green01: '#00c7ae',
   green02: '#C6F1EB',
