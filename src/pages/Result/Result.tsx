005e24bb4ba660bf8d93cda982dd5335425e2684
jjanmo <jjanmo07@hanmail.net>
2023-09-04 05:32:33 +0900

    fix: result 페이지에서 상태 구조 변경에 따른 사이드이펙트 수정 및 예외처리추가

--- src/pages/Result/Result.tsx
+++ src/pages/Result/Result.tsx
@@ -1,27 +1,50 @@
-import { shallowEqual, useSelector } from 'react-redux'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { RootState } from '@store/root'
 import { FormDataType } from '@store/slice/formSlice.type'
-import { ResultState } from '@store/slice/resultSlice.type'
+import { ResultDataType } from '@store/slice/resultSlice.type'
 import * as CS from '../common.style'
+import { resultActions } from '@store/slice/resultSlice'
+import { useEffect } from 'react'
 
 export default function Result() {
-  const form = useSelector<RootState, FormDataType | undefined>((state) => state.form.data)
-  const { formId, items } = useSelector<RootState, ResultState>((state) => state.result, shallowEqual)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const formData = useSelector<RootState, FormDataType | undefined>((state) => state.form.data)
+  const resultData = useSelector<RootState, ResultDataType>((state) => state.result.data, shallowEqual)
+  const { formId, items: resultItems } = resultData
+
+  useEffect(() => {
+    return () => {
+      dispatch(resultActions.resetResultData())
+    }
+  }, [dispatch])
+
+  if (!formData || !resultData) {
+    return (
+      <div>
+        매칭 요청서를 먼저 작성해주세요!
+        <button onClick={() => navigate('/')}>홈</button>
+      </div>
+    )
+  }
 
   return (
     <CS.Container>
       <CS.ContentWrapper>
+        <button onClick={() => navigate('/')}>홈</button>
+
         <h1>
-          <span>{form?.title}</span>에 대한 제출서 <span>({formId})</span>
+          <span>{formData?.title}</span>에 대한 제출서 <span>({formId})</span>
         </h1>
-        {form?.items.map((item) => (
+        {formData?.items.map((item) => (
           <div key={item.itemId}>
             <div>{item.title}</div>
             <div>
-              {items[item.itemId].map((optionId) => {
+              {resultItems[item.itemId].map((optionId) => {
                 const options = item.options
                 const selectedOption = options.find((option) => option.id === optionId)
-                return <div key={optionId}>{selectedOption?.text}</div>
+                return <span key={optionId}>{selectedOption?.text} </span>
               })}
             </div>
           </div>
