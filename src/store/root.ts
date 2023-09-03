2aa34a58b144fc7183b5f94e27866cabf967da71
jjanmo <jjanmo07@hanmail.net>
2023-09-03 18:11:33 +0900

    fix: dispatch 타입 오류 수정

--- src/store/root.ts
+++ src/store/root.ts
@@ -4,10 +4,6 @@ import logger from 'redux-logger'
 import formReducer from './slice/formSlice'
 import resultReducer from './slice/resultSlice'
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-export const useAppDispatch: () => AppDispatch = useDispatch
-
 const rootReducer = combineReducers({
   form: formReducer,
   result: resultReducer,
@@ -16,9 +12,12 @@ const rootReducer = combineReducers({
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => {
-    const middleware = [...getDefaultMiddleware()]
-    if (import.meta.env.NODE_ENV !== 'production') middleware.push(logger)
-    return middleware
+    if (import.meta.env.NODE_ENV !== 'production') return getDefaultMiddleware().concat(logger)
+    return getDefaultMiddleware()
   },
   devTools: import.meta.env.NODE_ENV !== 'production',
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export const useAppDispatch: () => AppDispatch = useDispatch
