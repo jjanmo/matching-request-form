526756fff5cda63656da0b7c254bc19490cd4523
jjanmo <jjanmo07@hanmail.net>
2023-09-03 21:05:24 +0900

    chores: theme color 정리

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,9 +4,7 @@ export const colors = {
   gray01: '#525252',
   gray02: '#999',
   gray03: '#e1e1e1',
-  cyan: '#17a2b8',
-  green: '#00c7ae',
+  green01: '#00c7ae',
+  green02: '#C6F1EB',
   red: '#ff3131',
-  blue: '#4785ff',
-  purple: '#6f42c1',
 }
